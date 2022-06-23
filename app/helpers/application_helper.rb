f1925998f9191befc1fbd4b814954abe8ffe1226
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-06-23 21:11:11 +0200

    Title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# Returns a full title on a per-page basis.
+
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = 'Orbit Feedback App'
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
--- test/controllers/html_pages_controller_test.rb
+++ test/controllers/html_pages_controller_test.rb
@@ -4,18 +4,18 @@ class HtmlPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get html_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Orbit"
+    assert_select "title", "Orbit Feedback App"
   end
 
   test "should get help" do
     get html_pages_help_url
     assert_response :success
-    assert_select "title", "Help | Orbit"
+    assert_select "title", "Help | Orbit Feedback App"
   end
 
   test "should get about" do
     get html_pages_about_url
     assert_response :success
-    assert_select "title", "About | Orbit"
+    assert_select "title", "About | Orbit Feedback App"
   end
 end
