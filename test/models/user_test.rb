ddeb15c8abb2a75ca063595999b8850e8baf5edb
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-07-08 18:45:24 +0200

    Add user_signup test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -51,13 +51,13 @@ class UserTest < ActiveSupport::TestCase
       assert_not duplicate_user.valid?
     end
 
-    test "password should be present (nonblank)" do
+    test "password should be present (not blank)" do
       @user.password = @user.password_confirmation = " " * 6
       assert_not @user.valid?
     end
 
     test "password should have a minimum length" do
-      @user.password = @user.password_confirmation = "a" * 6
+      @user.password = @user.password_confirmation = "p" * 5
       assert_not @user.valid?
     end
 end
